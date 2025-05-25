71d1820cdd93384df8dfc8ba4bfabfe8f2947449
StepanAndrosov <stepan.news@yandex.ru>
2025-05-25 12:27:55 +0300

    App setup

--- src/setup/swagger.setup.ts
+++ src/setup/swagger.setup.ts
@@ -1,11 +1,52 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { GLOBAL_PREFIX } from './global-prefix.setup';
+import { createWriteStream } from 'fs';
+import { get } from 'http';
 
 export function swaggerSetup(app: INestApplication) {
+  const SWAGGER_PATH = 'swagger';
+  const serverUrl = 'http://localhost:5050'
+  if (process.env.NODE_ENV === 'development') {
+    // write swagger ui files
+    get(
+      `${serverUrl}/swagger/swagger-ui-bundle.js`, function
+      (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+      console.log(
+        `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+      );
+    });
+
+    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+      console.log(
+        `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+      );
+    });
+
+    get(
+      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+      function (response) {
+        response.pipe(
+          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+        );
+        console.log(
+          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+        );
+      });
+
+    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+      console.log(
+        `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+      );
+    });
+
+  }
+
   const config = new DocumentBuilder()
     .setTitle('BLOGGER API')
-    .addBearerAuth()
+    .setDescription('The blogger API description')
     .setVersion('1.0')
     .addBearerAuth()
     .addBasicAuth(
@@ -18,7 +59,7 @@ export function swaggerSetup(app: INestApplication) {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(GLOBAL_PREFIX, app, document, {
+  SwaggerModule.setup(SWAGGER_PATH, app, document, {
     customSiteTitle: 'Blogger Swagger',
   });
 }
